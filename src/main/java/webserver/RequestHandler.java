1bd65d0a6af28e5c5fdbf8e952b3b441f9abc80a
lar542 <lar542@naver.com>
2018-04-12 15:26:42 +0900

    테스트 커밋

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -9,6 +9,9 @@ import java.net.Socket;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * 요청 처리와 응답 처리를 담당하는 클래스
+ */
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
@@ -22,7 +25,11 @@ public class RequestHandler extends Thread {
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
 
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+        try (
+        	//클라이언트에서 서버로 요청을 보낼 때 전달되는 데이터
+        	InputStream in = connection.getInputStream(); 
+        	//서버에서 클라이언트로 응답을 보낼 때 전달되는 데이터
+        	OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = "Hello World".getBytes();
--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -6,28 +6,33 @@ import java.net.Socket;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * WebServer 클래스는 웹 서버를 구동하고,
+ * 서버 구동 후 대기상태에서 요청이 들어오면
+ * 요청을 RequestHandler 클래스에 위임하는 역할을 한다.
+ */
 public class WebServer {
     private static final Logger log = LoggerFactory.getLogger(WebServer.class);
-    private static final int DEFAULT_PORT = 8080;
+    private static final int DEFAULT_PORT = 9090;
 
     public static void main(String args[]) throws Exception {
-        int port = 0;
+    	int port = 0;
         if (args == null || args.length == 0) {
             port = DEFAULT_PORT;
         } else {
             port = Integer.parseInt(args[0]);
         }
-
-        // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
-
+        
+        //서버를 구동하면 저장된 포트번호의 ServerSocket을 생성하고 대기
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             log.info("Web Application Server started {} port.", port);
 
-            // 클라이언트가 연결될때까지 대기한다.
-            Socket connection;
-            while ((connection = listenSocket.accept()) != null) {
-                RequestHandler requestHandler = new RequestHandler(connection);
-                requestHandler.start();
+            //클라이언트가 연결될때까지 대기한다.
+            Socket connection; 
+            while ((connection = listenSocket.accept()) != null) { //저장된 포트번호로 요청이 발생하면
+            	//Socket을 RequestHandler로 전달
+            	RequestHandler requestHandler = new RequestHandler(connection);
+                requestHandler.start(); //새로운 스레드 실행(멀티스레드 프로그래밍)
             }
         }
     }
